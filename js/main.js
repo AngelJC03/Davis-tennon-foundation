e8177b3ab99ebd34f3cfff3f36412ad70d064af2
angel <CAngel080803@gmail.com>
2024-12-29 23:44:40 -0500

    added two more pages (partners and princples). also changed the footer layout and removed the contact us page to just contain the contact info in the footer. The principles page is now titled 'about us' which briefly explains the principles (placeholder text).

--- js/main.js
+++ js/main.js
@@ -51,10 +51,35 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 /*********
+ THIS JAVASCRIPT IS FOR LOADING THE PARTNERS PAGE CONTENT
+ UNDER THE CONDITION THAT THE USER HAS SCROLLED TO THROUGH THE PAGE
+********/
+ 
+document.addEventListener('DOMContentLoaded', function() {
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                // When element is in view, add the 'in-view' class to trigger the animation
+                entry.target.classList.add('in-view');
+                observer.unobserve(entry.target); // Stop observing once the animation has been triggered
+            }
+        });
+    }, {
+        threshold: 0.5 // Trigger animation when 50% of the element is in view
+    });
+
+    // Observe each partner section
+    const partnerSections = document.querySelectorAll('.partner');
+    partnerSections.forEach(section => {
+        observer.observe(section);
+    });
+});
+
+/********
  * JAVASCRIPT FOR DONATION FORM
  * PROMPTING USER TO ENTER VALUE IN
  * AMOUNT FIELD
-    *********/
+ *********/
 
 function focusAmountInput() {
     document.getElementById('amount').value = ''; // Clears the amount input field
